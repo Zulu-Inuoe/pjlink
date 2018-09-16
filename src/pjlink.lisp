d9da276a8f725f3546bca76aca7ee6a9fc378f52
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2018-09-16 17:11:04 -0400

    Add support for powr?, inpt?, avmt?, and erst?
    Also switch code to use strings internally rather than octet vectors due to awkwardness converting to/from hex.

--- src/pjlink.lisp
+++ src/pjlink.lisp
@@ -34,159 +34,310 @@
     :initform (error "Must supply socket")
     :reader %socket)
    (%digest
-    :type (or null (simple-array (unsigned-byte 8) 32))
+    :type (or null (simple-array character 32))
     :initarg :digest
     :initform (error "Must supply digest")
-    :reader %digest)))
+    :reader %digest)
+   (%class
+    :type integer
+    :initarg :class
+    :initform (error "Must supply class")
+    :reader %class)))
+
+(defclass error-status ()
+  ((%fan-status
+    :type keyword
+    :initarg :fan
+    :initform (error "Must supply fan status")
+    :accessor fan-status)
+   (%lamp-status
+    :type keyword
+    :initarg :lamp
+    :initform (error "Must supply lamp status")
+    :accessor lamp-status)
+   (%temperature-status
+    :type keyword
+    :initarg :temperature
+    :initform (error "Must supply temperature status")
+    :accessor temperature-status)
+   (%cover-open-status
+    :type keyword
+    :initarg :cover-open
+    :initform (error "Must supply cover-open status")
+    :accessor cover-open-status)
+   (%filter-status
+    :type keyword
+    :initarg :filter
+    :initform (error "Must supply filter status")
+    :accessor filter-status)
+   (%other-status
+    :type keyword
+    :initarg :other
+    :initform (error "Must supply other status")
+    :accessor other-status)))
 
 (defun %encrypt-password (password seed &key (seed-start 0))
   "Create the authentication response given `password` and `seed`.
-Both `password` and `seed` should be octet sequences (vector (unsigned-byte 8))
-`password` can be 32 or fewer octets
-`seed` must be 4 octets."
-  (let ((digest (make-array 40 :element-type '(unsigned-byte 8))))
-    (declare (dynamic-extent digest))
-    (replace digest seed :start2 seed-start :end2 (+ seed-start 8))
-    (replace digest password :start1 8)
-    (let ((md5 (md5:md5sum-sequence digest :end (+ 8 (length password)))))
-      (loop
-        :with ret := (make-array 32 :element-type '(unsigned-byte 8))
-        :for i :from 0 :below 16
-        :for hex-d := (format nil "~(~2,'0X~)" (aref md5 i))
-        :do (setf (aref ret (+ (* i 2) 0)) (char-code (char hex-d 0))
-                  (aref ret (+ (* i 2) 1)) (char-code (char hex-d 1)))
-        :finally
-           (return ret)))))
+`password` should be a string length 32 or less
+`seed` should be a string, length 8
+`seed-start` denotes where to offset `seed`. 8 characters will be read from that offset."
+  (unless (<= (length password) 32)
+    (error "Password length too long"))
+  (let ((buffer (make-array 40 :element-type 'character)))
+    (declare (dynamic-extent buffer))
+    (replace buffer seed :start2 seed-start :end2 (+ seed-start 8))
+    (replace buffer password :start1 8)
+    (let ((md5 (md5:md5sum-string buffer :end (+ 8 (length password)))))
+      ;;We need to convert the md5 into a hex string
+      (with-output-to-string (str)
+        (loop
+          :for b :across md5
+          :do (format str "~(~2,'0X~)" b))))))
 
 (defun %coerce-password (password
-                         &key
-                           (external-format :utf-8)
-                           (start 0)
-                           (end nil end-sup-p)
                          &aux
                            (password (if (functionp password) (funcall password) password)))
-  "Coerces a password into a `(vector (unsigned-byte 8))`
-`password` can be a string, decoded according to `external-format`
-or an (array (unsigned-byte 8)) with maximum length 32."
+  "Coerces a password into a string`
+`password` can be a string, or a function of no arguments which returns a string."
   (etypecase password
     (string
-     (let ((ret (flexi-streams:string-to-octets password :external-format external-format :start start :end (if end-sup-p end (length password)))))
-       (unless (<= (length ret) 32)
-         (error "Encoded password length too long"))
-       ret))
-    ((vector (unsigned-byte 8) 32)
-     password)
-    ((array (unsigned-byte 8))
      (unless (<= (length password) 32)
        (error "Password length too long"))
      password)))
 
 (defun %verify-connect-response (response)
+  "Verifies the initial connection response.
+Returns nil if the response does not match a proper pjlink connection response."
   (and (>= (length response) 8)
-       (= (aref response 0) (char-code #\P))
-       (= (aref response 1) (char-code #\J))
-       (= (aref response 2) (char-code #\L))
-       (= (aref response 3) (char-code #\I))
-       (= (aref response 4) (char-code #\N))
-       (= (aref response 5) (char-code #\K))
-       (= (aref response 6) (char-code #\Space))
-       (or (and (= (aref response 7) (char-code #\0))
-                (>= (length response) 9)
-                (= (aref response 8) (char-code #\Return)))
-           (and (= (aref response 7) (char-code #\1))
-                (= (aref response 8) (char-code #\Space))
-                (>= (length response) 18)
-                (= (aref response 17) (char-code #\Return))))))
+       (string-equal response "PJLINK " :end1 7)
+       (or
+        ;; Authentication disabled
+        (and (char= (char response 7) #\0))
+        ;; Authentication enabled. Expect seed
+        (and (char= (char response 7) #\1)
+             (char= (char response 8) #\Space)
+             (>= (length response) 17)))))
 
-(defun %authentication-enabled (response)
-  (= (aref response 7) (char-code #\1)))
+(defun %create-digest (connection-response password)
+  "Creates a digest string from a connection response and a password.
+Empty string on a non-authenticated response.
+Otherwise calculates the response by prepending the seed from the connection response to the password.
 
-(defun %create-digest (response password)
+`connection-response` should be a string containing a PJLink connection response
+`password` should be a password designator"
   ;;Check whether authentication is enabled or not
-  (switch ((aref response 7))
-    ((char-code #\0)
-     ;;No authentication. Empty digest
-     (make-array 0 :element-type '(unsigned-byte 8)))
-    ((char-code #\1)
+  (ecase (char connection-response 7)
+    (#\0 "") ; No authentication. Empty digest
+    (#\1
      ;;Authentication
-     (%encrypt-password (%coerce-password password) response :seed-start 9))))
+     (%encrypt-password (%coerce-password password) connection-response :seed-start 9))))
 
 (defun %read-pjlink-command-line (buffer stream)
-  "Reads a pjlink command-line (delimited by 0x13) from `stream` into `buffer`"
+  "Reads a pjlink command-line (delimited by #\Return) from `stream` into `buffer`"
   (loop
-    :for b := (read-byte stream)
+    :for b := (read-char stream)
     :for i :from 0 :below (length buffer)
-    :do (setf (aref buffer i) b)
-    :until (= b (char-code #\return))))
+    ;; #\Return not included
+    :until (char= b #\Return)
+    :do (setf (char buffer i) b)
+    :finally
+    (return i)))
 
-(defmacro %with-command-buffer ((buffer connection command params) &body body)
-  (with-gensyms (conn command-sym params-sym idx)
-    `(let* ((,conn ,connection)
-            (,command-sym ,command)
-            (,params-sym ,params)
-            (,buffer (make-array 168 :element-type '(unsigned-byte 8)))
-            (,idx 0))
-       (declare (dynamic-extent ,command-sym ,params-sym ,buffer))
-       (replace ,buffer (%digest ,conn))
-       (incf ,idx (length (%digest ,conn)))
-       (setf (aref ,buffer (1- (incf ,idx))) (char-code #\%)
-             (aref ,buffer (1- (incf ,idx))) (char-code #\1))
-       (etypecase ,command-sym
-         (string
-          ,(with-gensyms (octets)
-             `(let ((,octets (flexi-streams:string-to-octets ,command-sym :external-format :utf-8)))
-                (replace ,buffer ,octets :start1 ,idx)
-                (incf ,idx (length ,octets))))))
-       (setf (aref ,buffer (1- (incf ,idx))) (char-code #\Space))
-       (etypecase ,params-sym
-         (string
-          ,(with-gensyms (octets)
-             `(let ((,octets (flexi-streams:string-to-octets ,params-sym :external-format :utf-8)))
-                (replace ,buffer ,octets :start1 ,idx)
-                (incf ,idx (length ,octets))))))
-       (setf (aref ,buffer (1- (incf ,idx))) (char-code #\Return))
+(defmacro %with-command-buffer ((buffer connection class command params) &body body)
+  "Create a buffer pre-filled with a PJLink command using `class`, `command` and `params`
+eg.
+  %1CLSS ?<Return>
+
+When authentication is enabled, also prepends the `connection`'s digest string."
+  `(%with-command-buffer-impl (,buffer (%digest ,connection) ,class ,command ,params)
+     ,@body))
+
+(defmacro %with-command-buffer-impl ((buffer digest class command params) &body body)
+    "Create a buffer pre-filled with a PJLink command using `digest`, `class`, `command` and `params`
+eg.
+  %1CLSS ?<Return>"
+  (with-gensyms (digest-sym class-sym command-sym params-sym idx)
+    `(let ((,digest-sym ,digest)
+           (,class-sym ,class)
+           (,command-sym ,command)
+           (,params-sym ,params)
+           (,buffer (make-array 168 :element-type 'character))
+           (,idx 0))
+       (declare (dynamic-extent ,buffer))
+       ;;Prepend the digest (if any)
+       (replace ,buffer ,digest-sym)
+       (incf ,idx (length ,digest-sym))
+
+       ;;Add %1
+       (setf (char ,buffer (1- (incf ,idx))) #\%
+             (char ,buffer (1- (incf ,idx))) ,class-sym)
+
+       ;;Add command type
+       (replace ,buffer ,command-sym :start1 ,idx)
+       (incf ,idx (length ,command-sym))
+
+       ;;Space
+       (setf (char ,buffer (1- (incf ,idx))) #\Space)
+
+       ;;Add params
+       (replace ,buffer ,params-sym :start1 ,idx)
+       (incf ,idx (length ,params-sym))
+
+       ;;End with #\Return
+       (setf (char ,buffer (1- (incf ,idx))) #\Return)
        (locally ,@body))))
 
 (defmacro %with-response-buffer (buffer &body body)
-  `(let* ((,buffer (make-array 136 :element-type '(unsigned-byte 8))))
+  `(let ((,buffer (make-array 136 :element-type 'character)))
      (declare (dynamic-extent ,buffer))
      ,@body))
 
-(defun %pjlink-print (buffer &optional (stream *standard-output*))
-  (let ((str (flexi-streams:octets-to-string buffer :external-format :utf-8)))
-    (loop
-      :for c :across str
-      :until (char= c #\Return)
-      :do (write-char c stream))
-    (terpri stream)
-    (values)))
+(defun %validate-get-result (class command response len)
+  (and (>= len 8)
+       (char= (char response 0) #\%)
+       (char= (char response 1) class)
+       (string-equal response command :start1 2 :end1 6)
+       (char= (char response 6) #\=)
+       (- len 7)))
 
-(defun %pjlink-clss? (connection &aux (stream (usocket:socket-stream (%socket connection))))
-  (%with-command-buffer (out connection "CLSS" "?")
-    (%pjlink-print out)
+(defun %powr->sym (input-val)
+  (ecase input-val
+    (#\0 :standby)
+    (#\1 :lamp-on)
+    (#\2 :cooling)
+    (#\3 :warm-up)))
+
+(defun %input->sym (input-val)
+  (ecase input-val
+    (#\1 :rgb)
+    (#\2 :video)
+    (#\3 :digital)
+    (#\4 :storage)
+    (#\5 :network)))
+
+(defun %avmt->sym (response)
+  (eswitch (response :test #'string-equal)
+    ("11" :vm-on)
+    ("21" :am-on)
+    ("31" :avm-on)
+    ("30" :avm-off)))
+
+(defun %erst->sym (erst)
+  (ecase erst
+    (#\0 :ok)
+    (#\1 :warning)
+    (#\2 :error)))
+
+(defun %pjlink-get-impl (stream digest class command)
+  "Conducts a `get` command on `stream`, and returns the result string
+uses
+ `digest` as the authorization digest
+ `class` as the command class
+ `command` as the command name
+
+
+This will issue a query such as
+  %1POWR ?
+
+Then given a result of
+  %1POWR=0
+returns the string \"0\""
+  (%with-command-buffer-impl (out digest class command "?")
     (write-sequence out stream)
     (finish-output stream)
     (%with-response-buffer in
-      (%read-pjlink-command-line in stream)
-      (%pjlink-print in))
-    0))
+      ;;Get the response params
+      (let* ((rlen (%read-pjlink-command-line in stream))
+             (param-len (%validate-get-result class command in rlen)))
+        (unless (null param-len)
+          (when (and (= param-len 4) (string-equal in "ERR" :start1 7 :end1 9))
+            (error
+             (ecase (char in 10)
+               (#\1 "Undefined command")
+               (#\3 "Unavailable time")
+               (#\4 "Projector/Display failure")
+               ((#\a #\A) "Authentication error"))))
+          (subseq in 7 (+ 7 param-len)))))))
 
-(defun %pjlink-connect (host
-                        &key
-                          (port +pjlink-port+)
-                          (password nil)
-                          (local-host nil)
-                          (local-port nil))
+(defun %pjlink-clss?-impl (stream digest)
+  "Lower level CLSS query used during initialization to verify authentication and device class."
+  (let ((result (%pjlink-get-impl stream digest #\1 "CLSS")))
+    (aref result 0)))
+
+(defun %pjlink-get (connection class command
+                    &aux (stream (usocket:socket-stream (%socket connection))))
+  "See `%pjlink-get-impl`. This is a convenience function taking in a connection instead."
+  (%pjlink-get-impl stream (%digest connection) class command))
+
+(defun pjlink-connect (host
+                       &key
+                         (port +pjlink-port+)
+                         (password nil)
+                         (local-host nil)
+                         (local-port nil))
+  "Open a PJLink connection to `host`.
+  `port` specifies the port to use. defaults to pjlink port 4352
+  `password` will be used if the host requires authentication
+  `local-host` allows specifying a different local host to use (interface device)
+  `local-port` allows specifying a specific local port to use"
   (let* ((socket (usocket:socket-connect host port
-                                         :element-type '(unsigned-byte 8)
+                                         :element-type 'character
                                          :local-host local-host
                                          :local-port local-port))
-         (stream (usocket:socket-stream socket))
-         (buffer (make-array 18 :element-type '(unsigned-byte 8))))
+         (buffer (make-array 18 :element-type 'character))
+         (stream (usocket:socket-stream socket)))
     (declare (dynamic-extent buffer))
     (%read-pjlink-command-line buffer stream)
     (unless (%verify-connect-response buffer)
       (error "Invalid response ~A" buffer))
 
-    (let ((digest (%create-digest buffer password)))
-      (make-instance 'pjlink-connection :socket socket :digest digest))))
+    (let* ((digest (%create-digest buffer password))
+           (class (%pjlink-clss?-impl stream digest)))
+      (make-instance '%pjlink-connection :socket socket :digest digest :class class))))
+
+(defun pjlink-powr? (connection)
+  (let ((result (%pjlink-get connection #\1 "POWR")))
+    (%powr->sym (char result 0))))
+
+(defun pjlink-inpt? (connection)
+  (let ((result (%pjlink-get connection #\1 "INPT")))
+    (values (%input->sym (aref result 0))
+            (parse-integer (string (code-char (aref result 1))) :radix 36))))
+
+(defun pjlink-avmt? (connection)
+  (let ((result (%pjlink-get connection #\1 "AVMT")))
+    (%avmt->sym result)))
+
+(defun pjlink-erst? (connection)
+  (let ((result (%pjlink-get connection #\1 "ERST")))
+    (make-instance
+     'error-status
+     :fan (%erst->sym (char result 0))
+     :lamp (%erst->sym (char result 1))
+     :temperature (%erst->sym (char result 2))
+     :cover-open (%erst->sym (char result 3))
+     :filter (%erst->sym (char result 4))
+     :other (%erst->sym (char result 5)))))
+
+(defun pjlink-lamp? (connection)
+  ;;Each lamp is returned as a pair of "<hour> SPC <status>" ordered by lamp number
+  (%pjlink-get connection #\1 "LAMP"))
+
+(defun pjlink-inst? (connection)
+  (%pjlink-get connection #\1 "INST"))
+
+(defun pjlink-name? (connection)
+  (%pjlink-get connection #\1 "NAME"))
+
+(defun pjlink-inf1? (connection)
+  (%pjlink-get connection #\1 "INF1"))
+
+(defun pjlink-inf2? (connection)
+  (%pjlink-get connection #\1 "INF2"))
+
+(defun pjlink-info? (connection)
+  (%pjlink-get connection #\1 "INFO"))
+
+(defun pjlink-clss? (connection)
+  (let ((result (%pjlink-get connection #\1 "CLSS")))
+    (values (parse-integer (string (char result 0)) :radix 16))))
